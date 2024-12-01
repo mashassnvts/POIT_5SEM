b36b2ddd9f7f4a45a02e0fdfe6af767ff8e01ba9
Maria Sosnovets <mariasosnovec@gmail.com>
2024-12-01 12:46:57 +0300

    added labs

---
+++ PSP/PSP_07/ClientMS/ClientMS/ClientMS.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include "Winsock2.h"
+#include <string.h>
+#include <ws2tcpip.h>
+#include <string>
+#include <vector>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
+#include <tchar.h>
+#include <chrono>
+using namespace std;
+#pragma comment(lib, "WS2_32.lib")
+
+//Задание 7
+void SendMessageToServers(char* serverName, char* message) {
+    HANDLE cH = CreateFileA(serverName, GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (cH == INVALID_HANDLE_VALUE) {
+        cerr << "Failed to create file. Error: " << GetLastError() << endl;
+    }
+    else
+    {
+        cout << "File created successfully" << endl;
+    }
+    
+    DWORD bytesSended;
+    if (!WriteFile(cH, message, strlen(message), &bytesSended, NULL)) {
+        cerr << "Failed to write to Mailslot. Error: " << GetLastError() << endl;
+    }
+    else {
+        cout << "Message sent successfully. Bytes sent: " << bytesSended << endl;
+    }
+
+    CloseHandle(cH);
+}
+
+int main() {
+    /*HANDLE cH = CreateFile(TEXT("\\\\MSI\\mailslot\\Box"), GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (cH == INVALID_HANDLE_VALUE) {
+        cerr << "Failed to create file. Error: " << GetLastError() << endl;
+        return 1;
+    }
+    else {
+        cout << "File created successfully" << endl;
+    }*/
+
+    //Задание 9
+   /* auto start = chrono::high_resolution_clock::now();
+    for (int i = 0; i < 1000; ++i) {
+        char message[50];
+        sprintf(message, "Message %d", i);
+        DWORD bytesSended;
+
+        if (!WriteFile(cH, message, strlen(message)+1, &bytesSended, NULL)) {
+            cerr << "Error sending message: " << GetLastError() << " - " << message << endl;
+            break;
+        }
+        char m[50];
+        sprintf(m, "message %d sent", i);
+        cout << m << endl;
+        Sleep(5);
+    }
+
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start;
+    cout << "Time taken to send 1000 messages: " << duration.count() << " seconds" << endl;*/
+    
+    
+    //char message[50] = "Hello";
+    /*DWORD bytesSended;
+    if (!WriteFile(cH, message, strlen(message) + 1, &bytesSended, NULL)) {
+        cerr << "Error sending message: " << GetLastError() << " - " << message << endl;
+    }
+    CloseHandle(cH);*/
+
+    char nameServer[20] = "\\\\MSI\\mailslot\\Box";
+    char message[50] = "Hello from client";
+    SendMessageToServers(nameServer, message);
+
+    char nameServer2[40] = "\\\\DESKTOP-0M3BPJP\\mailslot\\Box";
+    char message2[50] = "Hello from client";
+    SendMessageToServers(nameServer2, message2);
+    return 0;
+}
---
+++ PSP/PSP_07/ServerMS/ServerMS/ServerMS.cpp
@@ -0,0 +1,160 @@
+﻿#include <iostream>
+#include "Winsock2.h"
+#include <string.h>
+#include <ws2tcpip.h>
+#include <string>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 4996)
+#include <tchar.h>
+using namespace std;
+#pragma comment(lib, "WS2_32.lib")
+
+string SetErrorMsgText(string msg, int errorNumber) {
+    string result = "";
+    switch (errorNumber)
+    {
+    case WSAEINTR:
+        result = msg + "Работа функции прервана WSAITR";
+    case WSAEACCES:
+        result = msg + "Разрешение отвергнутоWSAEACCES";
+    case WSAEFAULT:
+        result = msg + "Ошибочный адрес  WSAEFAULT";
+    case WSAEINVAL:
+        result = msg + "Ошибка в аргументеWSAEINVAL";
+    case WSAEMFILE:
+        result = msg + "Слишком много файлов открытоWSAEMFILE";
+    case WSAEWOULDBLOCK:
+        result = msg + "Ресурс временно недоступенWSAEWOULDBLOCK";
+    case WSAEINPROGRESS:
+        result = msg + "Операция в процессе развитияWSAEINPROGRESS";
+    case WSAEALREADY:
+        result = msg + "Операция уже выполняется WSAEALREADY";
+    case WSAENOTSOCK:
+        result = msg + "Сокет задан неправильно  WSAENOTSOCK ";
+    case WSAEDESTADDRREQ:
+        result = msg + "Требуется адрес расположения WSAEDESTADDRREQ ";
+    case WSAEMSGSIZE:
+        result = msg + "Сообщение слишком длинное ";
+    case WSAEPROTOTYPE:
+        result = msg + "Неправильный тип протокола для сокета ";
+    case WSAENOPROTOOPT:
+        result = msg + "Ошибка в опции протокола";
+    case WSAEPROTONOSUPPORT:
+        result = msg + "Протокол не поддерживается";
+    case WSAESOCKTNOSUPPORT:
+        result = msg + "Тип сокета не поддерживается ";
+    case WSAEOPNOTSUPP:
+        result = msg + "Операция не поддерживается";
+    case WSAEPFNOSUPPORT:
+        result = msg + "Тип протоколов не поддерживается ";
+    case WSAEAFNOSUPPORT:
+        result = msg + "Тип адресов не поддерживается протоколом";
+    case WSAEADDRINUSE:
+        result = msg + "Адрес уже используется ";
+    case WSAEADDRNOTAVAIL:
+        result = msg + "Запрошенный адрес не может быть использован";
+    case WSAENETDOWN:
+        result = msg + "Сеть отключена ";
+    case WSAENETUNREACH:
+        result = msg + "Сеть не достижима";
+    case WSAENETRESET:
+        result = msg + "Сеть разорвала соединение";
+    case WSAECONNABORTED:
+        result = msg + "Программный отказ связи";
+    case WSAECONNRESET:
+        result = msg + "Связь восстановлена ";
+    case WSAENOBUFS:
+        result = msg + "Не хватает памяти для буферов";
+    case WSAEISCONN:
+        result = msg + "Сокет уже подключен";
+    case WSAENOTCONN:
+        result = msg + "Сокет не подключен";
+    case WSAESHUTDOWN:
+        result = msg + "Нельзя выполнить send: сокет завершил работу";
+    case WSAETIMEDOUT:
+        result = msg + "Закончился отведенный интервал  времени";
+    case WSAECONNREFUSED:
+        result = msg + "Соединение отклонено";
+    case WSAEHOSTDOWN:
+        result = msg + "Хост в неработоспособном состоянии";
+    case WSAEHOSTUNREACH:
+        result = msg + "Нет маршрута для хоста ";
+    case WSAEPROCLIM:
+        result = msg + "Слишком много процессов ";
+    case WSASYSNOTREADY:
+        result = msg + "Сеть не доступна";
+    case WSAVERNOTSUPPORTED:
+        result = msg + "Данная версия недоступна ";
+    case WSANOTINITIALISED:
+        result = msg + "Не выполнена инициализация WS2_32.DLL";
+    case WSAEDISCON:
+        result = msg + "Выполняется отключение";
+    case WSATYPE_NOT_FOUND:
+        result = msg + "Класс не найден ";
+    case WSAHOST_NOT_FOUND:
+        result = msg + "Хост не найден";
+    case WSATRY_AGAIN:
+        result = msg + "Неавторизированный хост не найден ";
+    case WSANO_RECOVERY:
+        result = msg + "Неопределенная  ошибка";
+    case WSANO_DATA:
+        result = msg + "Нет записи запрошенного типа ";
+    case WSA_INVALID_HANDLE:
+        result = msg + "Указанный дескриптор события  с ошибкой";
+    case WSA_INVALID_PARAMETER:
+        result = msg + "Один или более параметров с ошибкой";
+    case WSA_IO_INCOMPLETE:
+        result = msg + "Объект ввода-вывода не в сигнальном состоянии";
+    case WSA_IO_PENDING:
+        result = msg + "Операция завершится позже";
+    case WSA_NOT_ENOUGH_MEMORY:
+        result = msg + "Не достаточно памяти ";
+    case WSA_OPERATION_ABORTED:
+        result = msg + "Операция отвергнута";
+    case WSASYSCALLFAILURE:
+        result = msg + "Аварийное завершение системного вызова";
+    default:
+        break;
+    }
+    return result;
+}
+
+int main(){
+
+    HANDLE sH = CreateMailslot(TEXT("\\\\.\\mailslot\\Box"), 500, 180000, NULL);
+    cout << "server listening..." << endl;
+    if (sH == INVALID_HANDLE_VALUE) {
+        throw SetErrorMsgText("create: ", GetLastError());
+    }
+
+    char rbuf[200];
+    DWORD bytesRead;
+    if (!ReadFile(sH, rbuf, sizeof(rbuf)-1, &bytesRead, NULL)) {
+        DWORD error = GetLastError();
+        if (error == ERROR_SEM_TIMEOUT) {
+            cout << "Timeout expired while reading from Mailslot." << endl;
+        }
+        else {
+            throw SetErrorMsgText("read: ", GetLastError());
+        }
+    }
+    else {
+        //rbuf[bytesRead] = '\0';
+        cout << bytesRead << endl;
+        cout << "Received message: " << rbuf << endl;
+    }
+
+    //Задание 9
+//    while (true) {
+//    if (!ReadFile(sH, rbuf, sizeof(rbuf), &bytesRead, NULL)) {
+//        cerr << "Error reading from mailslot: " << GetLastError() << endl;
+//        continue;
+//    }
+//    rbuf[bytesRead] = '\0';
+//    cout << "Received message: " << rbuf << endl;
+//}
+//
+//    CloseHandle(sH);
+     
+    return 0;
+}
